5f3af1cb78543a666d4c98d85d539cc2c513fe33
WinRobotics <sherwinfun@gmail.com>
2023-08-12 22:49:10 +0800

    concept test ok

--- 2215/2215.cpp
+++ 2215/2215.cpp
@@ -1,37 +1,53 @@
+
 #include <iostream>
 #include <map>
 #include <unordered_map>
 #include <vector>
 #include <iterator>
 using namespace std;
-int main (){
-  vector<int> v ={1,2,3,5};
-  vector<int> w ={3,4,1,2};
-  unordered_map<int,int> first;
-  
-  //initializing
-  first[v[0]]=0;
-  first[v[1]]=0;
-  first[v[2]]=0;
-  first[v[3]]=0;
-  
-   unordered_map<int, int>::iterator it;
-   
-   for(int i : w)
-   {
-        it = first.find(i);
-        if(it != first.end())
+
+
+class Solution
+{
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+
+    //what if the vectors are uneven?
+    unordered_map<int,int> diff1,diff2;
+    unordered_map<int,int>::iterator it1,it2;
+
+    //filling up the key
+    for(int i =0; i<nums1.size();i++)
+    {
+        diff1[nums1[i]];
+    }
+
+    for(int p =0; p<nums2.size();p++)
+    {
+        diff2[nums2[p]];
+    }
+
+    for(int z : nums1)
+    {
+
+        it1 = diff2.find(z);
+        if(it1 != diff2.end())
         {
-            it->second++;
-        }
-        else
-        {
-            first.insert(std::make_pair(i,0));
-        }
-   }
-   for(it=first.begin(); it!=first.end(); ++it){
-      cout << it->first << " => " << it->second << '\n';
-   }
-   
-  return 0;
+            std::cout<<"z appear : "<<z<<std::endl;
+        }        
+    }
+
+    
+    }
+};
+
+
+int main()
+{
+
+    Solution n;
+    vector<int> num1 = {1,2,3};
+    vector<int> num2 = {4,3,2};
+    n.findDifference(num1,num2);
+
 }
