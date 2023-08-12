d6e4a0a8e423dceabe621f881798c6ee824d1bd2
WinRobotics <sherwinfun@gmail.com>
2023-08-12 22:17:28 +0800

    concept code

--- 2215/2215.cpp
+++ 2215/2215.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
+#include <map>
+#include <unordered_map>
 #include <vector>
-
+#include <iterator>
 using namespace std;
-
-class Solution {
-public:
-
-    
-};
-int main()
-{
-    Solution n;
-
-
+int main (){
+  vector<int> v ={1,2,3,5};
+  vector<int> w ={3,4,1,2};
+  unordered_map<int,int> first;
+  
+  //initializing
+  first[v[0]]=0;
+  first[v[1]]=0;
+  first[v[2]]=0;
+  first[v[3]]=0;
+  
+   unordered_map<int, int>::iterator it;
+   
+   for(int i : w)
+   {
+        it = first.find(i);
+        if(it != first.end())
+        {
+            it->second++;
+        }
+        else
+        {
+            first.insert(std::make_pair(i,0));
+        }
+   }
+   for(it=first.begin(); it!=first.end(); ++it){
+      cout << it->first << " => " << it->second << '\n';
+   }
+   
+  return 0;
 }
